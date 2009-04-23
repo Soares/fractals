56845c0f9a7bbb9f1b7ed9baee870c4cfe9fdb21
Nate <nate@natesoares.com>
2009-04-23 19:01:53 -0400

    Added the LSystem class

--- fractals.py
+++ fractals.py
@@ -44,3 +44,41 @@ class GeneratorList(object):
         for _ in range(index - len(self.__list) + 1):
             self.__list.append(self.__generator.next())
         return self.__list[index]
+
+
+class LSystem(GeneratorList):
+    """
+    A class capable of actually doing something with L-System iteration strings
+    made from lendenmayer.
+
+    The drawing is done by looking up each character in the object's "action"
+    dictionary. By default, the following actions are expected to be defined by
+    implementors:
+
+    forward: called on the character 'F'. Draw one line
+    right: called on the charcater '+'. Turn right by the object's "angle"
+    left: called on the character '-'. Turn left by the object's "angle"
+    go: called on the charater 'G'. Move forward, without drawing a line
+    save: called on the character '['. Save the current drawing state in
+          the object's "states" list
+    restore: called on the character ']'. Restore the drawing state from
+             the object's "states" list.
+    """
+    def __init__(self, axiom, rules, angle, heading=0):
+        self.states = []
+        self.angle = angle
+        self.heading = heading
+        self.actions = {
+            'F': self.forward,
+            '+': self.right,
+            '-': self.left,
+            'G': self.go,
+            '[': self.save,
+            ']': self.restore,
+        }
+        super(LSystem, self).__init__(lindenmayer(axiom, rules))
+
+    def draw(self, index):
+        for char in self[index]:
+            if char in self.actions:
+                self.actions[char]()
