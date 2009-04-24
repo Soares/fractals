f764c41948c2507c8a7af73b2701a1832b19edfa
Nate <nate@natesoares.com>
2009-04-23 22:32:06 -0400

    Added color wheel and tests

--- fractals.py
+++ fractals.py
@@ -1,3 +1,42 @@
+class ColorWheel:
+    RED, YELLOW, GREEN, TEAL, BLUE, PURPLE = range(6)
+
+    __modes = {
+        'red': ((255, 0, 0), 1, 1),
+        'yellow': ((255, 255, 0), 0, -1),
+        'green': ((0, 255, 0), 2, 1),
+        'teal': ((0, 255, 255), 1, -1),
+        'blue': ((0, 0, 255), 0, 1),
+        'purple': ((255, 0, 255), 2, -1),
+    }
+
+    def __init__(self, color=RED, mode=255):
+        self.mode = mode
+        self.goto(color)
+
+    def clamp(self, n):
+        return min(self.mode, max(0, n))
+
+    def goto(self, color):
+        if not isinstance(color, basestring) \
+           or color.lower() not in self.__modes:
+            color = Red
+        color, self.channel, self.direction = self.__modes[color]
+        self.current = list(color)
+
+    def rotate(self, amount):
+        current = self.current[self.channel]
+        if current == self.mode and self.direction > 0:
+            self.direction *= -1
+            self.channel = (self.channel - 1) % 3
+        elif current == 0 and self.direction < 0:
+            self.direction *= -1
+            self.channel = (self.channel + 2) % 3
+        delta = self.direction * amount
+        self.current[self.channel] = self.clamp(current + delta)
+        return tuple(self.current)
+
+
 def lindenmayer(axiom, rules):
     """
     Given a start axiom (string) and a set of rules (dictionary),
@@ -120,17 +159,52 @@ class LSystem(GeneratorList):
                 self.actions[char]()
 
 
+class Dragon(LSystem):
+    def __init__(self, turtle):
+        super(Serpinsky, self).__init__(turtle, 'FX', {'X': 'X+YF', 'Y': 'FX-Y'}, 90)
+        self.colors = ColorWheel('purple')
+        self.__cap, self.__current = 1, 0
+
+    def update(self):
+        if self.__cap == self.__current:
+            self.__cap *= 2
+            self.turtle.pencolor(self.colors.rotate(40))
+        self.__current += 1
+
+    def draw(self, *args, **kwargs):
+        self.turtle.pencolor(self.colors.current)
+        super(Dragon, self).draw(*args, **kwargs)
+
+
+class Serpinsky(LSystem):
+    def __init__(self, turtle):
+        super(Serpinsky, self).__init__(turtle, 'FA', {'FA': 'FB-FA-FB', 'FB': 'FA+FB+FA'}, 60)
+        self.colors = ColorWheel('yellow')
+        self.__cap, self.__current = 1, 0
+
+    def update(self):
+        if self.__cap == self.__current:
+            self.__cap *= 3
+            self.turtle.pencolor(self.colors.rotate(100))
+        self.__current += 1
+
+    def draw(self, *args, **kwargs):
+        self.turtle.pencolor(self.colors.current)
+        super(Serpinsky, self).draw(*args, **kwargs)
+
+
 if __name__ == '__main__':
     from turtle import Turtle
     turtle = Turtle()
     turtle.hideturtle()
     turtle.speed('fastest')
+    turtle.screen.colormode(255)
 
     snowflake = LSystem(turtle, 'F++F++F', {'F': 'F-F++F-F'}, 60)
     dragon = LSystem(turtle, 'FX', {'X': 'X+YF', 'Y': 'FX-Y'}, 90)
     plant = LSystem(turtle, 'FX', {'X': 'F-[[X]+X]+F[+FX]-X', 'F': 'FF'}, 25)
     serpinsky = LSystem(turtle, 'FA', {'FA': 'FB-FA-FB', 'FB': 'FA+FB+FA'}, 60)
 
-    dragon.draw(10)
+    Serpinsky(turtle).draw(9)
 
     turtle.screen.exitonclick()
